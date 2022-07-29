33d34729b012c97a9f29db2a35a4bf7eb5e68f12
huhailong <huhailong@kingsoft.com>
2022-07-29 16:43:56 +0800

    fix: StackInfo 代码整理

--- Topic/Topic6/StackInfo.h
+++ Topic/Topic6/StackInfo.h
@@ -2,65 +2,67 @@
 
 struct Node
 {
-	void* Data = nullptr;
-	Node* Next = nullptr;
+    void* Data = nullptr;
+    Node* Next = nullptr;
 };
 
 class StackInfo
 {
 public:
-	~StackInfo()
-	{
-		Node* Temp = Top;
-		while (Temp)
-		{
-			Temp = Top->Next;
-			delete Top->Data;
-			delete Top;
-		}
-		delete Temp;
-		Temp = nullptr;
-		Top = nullptr;
-	}
+    ~StackInfo()
+    {
+        Node* Temp = Top;
 
-	template <typename T>
-	void Push(const T& t);
+        while (Temp)
+        {
+            Temp = Top->Next;
+            delete Top->Data;
+            delete Top;
+        }
 
-	template <typename T>
-	void Pop(T& t);
+        delete Temp;
+        Temp = nullptr;
+        Top = nullptr;
+    }
+
+    template <typename T>
+    void Push(const T& t);
+
+    template <typename T>
+    void Pop(T& t);
 
 private:
-	Node* Top = nullptr;
+    Node* Top = nullptr;
 };
 
 template <typename T>
 void StackInfo::Push(const T& Value)
 {
-	T* P = new T(Value);
-	Node* NewOne = new Node();
-	NewOne->Data = (void*)P;
+    T* P = new T(Value);
+    Node* NewOne = new Node();
+    NewOne->Data = (void*)P;
 
-	if (Top == nullptr)
-	{
-		Top = NewOne;
-		return;
-	}
+    if (Top == nullptr)
+    {
+        Top = NewOne;
+        return;
+    }
 
-	NewOne->Next = Top;
-	Top = NewOne;
+    NewOne->Next = Top;
+    Top = NewOne;
 }
 
 template <typename T>
 void StackInfo::Pop(T& Data)
 {
-	if (Top == nullptr)
-	{
-		return;
-	}
+    if (Top == nullptr)
+    {
+        return;
+    }
 
-	Node* Temp = Top->Next;
-	Data = *(T*)Top->Data;
-	delete Top->Data;
-	delete Top;
-	Top = Temp;
+    Node* Temp = Top->Next;
+    Data = *(T*)Top->Data;
+    delete Top->Data;
+    delete Top;
+    Top = Temp;
 }
--- Topic/Topic6/main.cpp
+++ Topic/Topic6/main.cpp
@@ -4,53 +4,54 @@
 class Test
 {
 public:
-	int P1 = 1;
-	bool P2 = false;
-	std::string P3 = "p33333333";
+    int P1 = 1;
+    bool P2 = false;
+    std::string P3 = "p33333333";
 
-	void TestWithStack(StackInfo& Stack)
-	{
-		int V1 = 0;
-		std::string V2;
-		Test V3;
-		Test* V4 = nullptr;
+    void TestWithStack(StackInfo& Stack)
+    {
+        int V1 = 0;
+        std::string V2;
+        Test V3;
+        Test* V4 = nullptr;
 
-		Stack.Pop<Test*>(V4);
-		Stack.Pop(V3);
-		Stack.Pop(V2);
-		Stack.Pop(V1);
+        Stack.Pop<Test*>(V4);
+        Stack.Pop(V3);
+        Stack.Pop(V2);
+        Stack.Pop(V1);
 
-		std::cout << "TestWithStack "
-			<< V1 << " "
-			<< V2 << " "
-			<< V3.P3 << " "
-			<< V4->P3 << std::endl;
+        std::cout << "TestWithStack "
+            << V1 << " "
+            << V2 << " "
+            << V3.P3 << " "
+            << V4->P3 << std::endl;
 
-		Stack.Push(1);
-	}
+        Stack.Push(1);
+    }
 };
 
 void TestFunc()
 {
-	Test T1;
-	StackInfo Stack;  // 实现此类，就俩模板函数，Push和Pop
-	Stack.Push(111);
-	Stack.Push<std::string>("test string");
+    Test T1;
+    StackInfo Stack;  // 瀹炵幇姝ょ被锛屽氨淇╂ā鏉垮嚱鏁帮紝Push鍜孭op
+    Stack.Push(111);
+    Stack.Push<std::string>("test string");
 
-	Test T2;
-	T2.P3 = "t2teststring";
-	Stack.Push(T2);
-	Stack.Push(&T1);
+    Test T2;
+    T2.P3 = "t2teststring";
+    Stack.Push(T2);
+    Stack.Push(&T1);
 
-	T1.TestWithStack(Stack);
+    T1.TestWithStack(Stack);
 
-	int Ret = 0;
-	Stack.Pop(Ret);
-	std::cout << "TestWithStack Ret " << Ret << std::endl;
+    int Ret = 0;
+    Stack.Pop(Ret);
+    std::cout << "TestWithStack Ret " << Ret << std::endl;
 }
 
 int main()
 {
-	TestFunc();
-	return 0;
+    TestFunc();
+
+    return 0;
 }
